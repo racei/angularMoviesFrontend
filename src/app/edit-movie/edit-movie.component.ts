d2b2f5c359866fd89372b19da7ffbfd17aaea349
Stephen Panzer <radicaledward@gmail.com>
2018-08-12 14:06:17 -0600

    More work on edit movie component

--- src/app/edit-movie/edit-movie.component.ts
+++ src/app/edit-movie/edit-movie.component.ts
@@ -3,7 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../movie.service';
 import { Location } from '@angular/common';
 import { Movie } from '../Movie';
-
+import { User } from '../User';
+import { UsersService } from '../users.service';
+declare var jquery:any;
 @Component({
   selector: 'app-edit-movie',
   templateUrl: './edit-movie.component.html',
@@ -11,16 +13,32 @@ import { Movie } from '../Movie';
 })
 export class EditMovieComponent implements OnInit {
   movie: Movie;
+  allUsers: User[];
+  filteredUsers: User[];
   constructor(
     private route: ActivatedRoute,
     private movieService: MovieService,
-    private location: Location
+    private location: Location,
+    private userService: UsersService
   ) { }
 
   ngOnInit() {
+    this.getMovie();
+    this.getUsers();
+    jquery('.selectpicker').selectpicker();
+  }
+
+  getUsers(){
+    this.userService.getAllUsers().subscribe((data) => {
+      this.allUsers = data;
+    });
+  }
+
+  getMovie(){
     const id = +this.route.snapshot.paramMap.get('id');
     this.movieService.getMovie(id).subscribe((data) => {
       this.movie = data;
+      this.filteredUsers = data.usersWatched;
     });
   }
 
