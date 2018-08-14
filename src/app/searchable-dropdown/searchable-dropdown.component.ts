a889507c87856a7114aa206b30d22e8e3ca56824
Stephen Panzer <radicaledward@gmail.com>
2018-08-14 16:48:02 -0600

    Some small refactoring

--- src/app/searchable-dropdown/searchable-dropdown.component.ts
+++ src/app/searchable-dropdown/searchable-dropdown.component.ts
@@ -18,17 +18,31 @@ export class SearchableDropdownComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  private sortOptions(x,y){
+
+      if(x.name < y.name)
+        return -1;
+      if(x.name > y.name)
+        return 1;
+      return 0;
+  }
+
+  private setFilteredOptions(){
+
+    this.filteredOptions = this.options.sort(this.sortOptions);
+  }
   ngAfterContentInit(){
-    this.filteredOptions = this.options;
+    this.setFilteredOptions();
   }
 
   ngOnChanges(){
-    this.filteredOptions = this.options;
+    this.setFilteredOptions();
   }
 
 
   filterValues(){
-    this.filteredOptions = this.options.filter((ele) => ele.name.toLowerCase().includes(this.searchText.toLowerCase()))
+    this.filteredOptions = this.options.filter((ele) => ele.name.toLowerCase().includes(this.searchText.toLowerCase())).sort(this.sortOptions);
   }
 
   addUser(user: User){
