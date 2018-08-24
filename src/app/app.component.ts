ff35ea343ae936f5589f4f346da2f647b9950e7c
Stephen Panzer <radicaledward@gmail.com>
2018-08-24 14:22:06 -0600

    Better app drawer/navbar animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,12 +17,11 @@ import {
       state('open', style({
         opacity: '1',
         display: 'block',
-        transform: 'translate3d(0, 0, 0)'
+        height: '*'
       })),
       state('closed',   style({
-        opacity: '0',
         display: 'none',
-        transform: 'translate3d(0, -100%, 0)'
+        height: '0'
       })),
       transition('closed => open', animate('200ms ease-in')),
       transition('open => closed', animate('100ms ease-out'))
